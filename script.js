5c344d1f55672363af143511f0855729c05b0341
mchlol <m.andreabr@gmail.com>
2021-11-11 17:47:55 +1100

    updates

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-const screen = document.querySelector(".screen-bottom");
-let currentValue = "2";
-screen.textContent = currentValue;
+const screenMain = document.querySelector(".screen-main");
+let currentMainValue = "2";
+screenMain.textContent = currentMainValue;
+
+const screenTop = document.querySelector('.screen-top');
+let currentTopValue = "1 + 1";
+screenTop.textContent = currentTopValue;
+
+let currentOperand;
 
 const buttons = document.querySelectorAll(".btn");
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
       console.log(button.id);
-      if(button.classList.contains("number-btn")) {
-      currentValue = button.textContent;
-      screen.textContent = currentValue;
-      }
-      else {
-          if (button.classList.contains("function-btn")) {
-              if (button.id = "add") {
-                  // currentValue plus next value?
-              }
-          }
-      }
+      screenMain.textContent = button.id;
     })
-  });
+});
 
+/*
+click button
+if number, display on screenMain
+if there is a number on screenMain, next number should append to it's right
+    if (screen contains numbers) { screenValue + button.id}
+if operand is clicked, move first number to top of screen
+    operands: add, subtract, divide, multiply
+store top of screen contents in currentTopValue
+when = is clicked, calculate currentTopValue
+if screenTop is 1 + 1, if the next button is a number append it (eg 1+1 becomes 1+12) OR if it's an operator, calculate (1+1) and display that on screenMain
+* limit amount of numbers than can appear on the screen - round longer numbers?
+* disable decimal if there's already one on the screen
+    if (screen contains decimal) { disable decimal button }
+* pressing clear should reset all 'current' variables
+* extra credit: decimals, backspace, keyboard support
+*/
 
-  /* 
-  click button
-  if number button display on screen
-  if function button perform function
-  store screen value in variable
-  */
-
+function calculate(number1, operand, number2) {
+    if (operand === add) {
+        return number1 + number2;
+    } else if (operand === subtract) {
+        return number1 - number2;
+    } else if (operand === multiply) {
+        return number1 * number2;
+    } else if (operand === divide) {
+        return number1 / number2;
+    } else {
+        return "80085";
+    }
+};

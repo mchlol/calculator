0ba2ba87eae54c74e37cba49ddcaab17ee5436c5
mchlol <m.andreabr@gmail.com>
2021-11-26 22:23:29 +1100

    calculator functioning with array

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let currentMainValue = 0;
 screenMain.textContent = currentMainValue;
 
 const screenTop = document.querySelector('.screen-top');
-let currentTopValue = 0;
+let currentTopValue = [];
 screenTop.textContent = currentTopValue;
 
 let currentOperand = null;
+let operandActive = false;
+let decimalActive = false;
 let newValue;
 
 const buttons = document.querySelectorAll('.btn');
@@ -19,36 +21,56 @@ const buttons = document.querySelectorAll('.btn');
 // const equalsBtn = document.querySelector('[data-equals');
 
 
-const add = function add(number1, number2) {
-    return number1 + number2;
+const add = function add(num1, num2) {
+    return Number(num1) + Number(num2);
 }
 
-const subtract = function subtract(number1, number2) {
-    return number1 - number2;
+const subtract = function subtract(num1, num2) {
+    return Number(num1) - Number(num2);
 }
 
-const multiply = function multiply(number1, number2) {
-    return number1 * number2;
+const multiply = function multiply(num1, num2) {
+    return Number(num1) * Number(num2);
 }
 
-const divide = function divide(number1, number2) {
-    return number1 / number2;
+const divide = function divide(num1, num2) {
+    return Number(num1) / Number(num2);
 }
 
-function operate(operand, number1, number2) {
-    switch(operand) {
-        case add:
-            return add(number1,number2);
-        case subtract:
-            return subtract(number1,number2);
-        case multiply:
-            return multiply(number1,number2);
-        case divide:
-            return divide(number1,number2);
+// stack can be an array eg. [1, “*”, 1]
+
+// ## FUNCTION USING ARRAY STACK
+function operate(array) {
+    switch(array[1]) {
+        case "+":
+            return add(array[0],array[2]);
+        case "-":
+            return subtract(array[0],array[2]);
+        case "*":
+            return multiply(array[0],array[2]);
+        case "/":
+            return divide(array[0],array[2]);
         default: 
-            console.log("no operand");
+            console.log("no operand input");
     }
 }
+
+
+// ## FUNCTION USING VARIABLES
+// function operate(num1, operand, num2) {
+//     switch(operand) {
+//         case add:
+//             return add(num1,num2);
+//         case subtract:
+//             return subtract(num1,num2);
+//         case multiply:
+//             return multiply(num1,num2);
+//         case divide:
+//             return divide(num1,num2);
+//         default: 
+//             console.log("no operand input");
+//     }
+// }
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
@@ -59,25 +81,79 @@ buttons.forEach(button => {
 // max numbers = 10
 function handleClick(button) {
     if (button.id === 'allclear') {
+        decimalActive = false;
         currentMainValue = 0;
-        currentTopValue = 0;
+        currentTopValue = [];
         screenMain.textContent = currentMainValue;
         screenTop.textContent = currentTopValue;
+
     } else if (button.id === 'clear') {
+        decimalActive = false;
         clear(currentMainValue);
+
+    } else if ('decimal' in button.dataset) {
+        if (decimalActive === true) {
+            console.log(`Decimal point present: ${decimalActive}`);
+        } else {
+        decimalActive = true;
+        decimalPoint(currentMainValue);
+        }
+
     } else if ('number' in button.dataset) { 
+        // check currentTopValue is true (contains a value)
         if (currentMainValue === 0) {
             currentMainValue = button.id;
             screenMain.textContent = currentMainValue;
-        } else if (currentMainValue === NaN) { // if value is NaN ?
-            currentMainValue = 0;
-            screenMain.textContent = currentMainValue;
+        // } else if (decimalActive) {
+        //     let decimalHandler = currentMainValue += button.id;
+        //     currentMainValue = Number(decimalHandler);
+        //     screenMain.textContent = currentMainValue;
+
         } else {
         currentMainValue += button.id;
         screenMain.textContent = currentMainValue;
         }
+
+    } else if ('operand' in button.dataset) {
+        currentOperand = button.id;
+        console.log(`Current operand is now ${currentOperand}`);
+        currentTopValue.push(currentMainValue);
+        currentTopValue.push(currentOperand);
+        screenTop.textContent = currentTopValue;
+        currentMainValue = 0;
+        screenMain.textContent = currentMainValue;
+        // clear the currentMainValue 
+
+    } else if ('equals' in button.dataset) {
+        currentTopValue.push(currentMainValue);
+        screenTop.textContent = currentTopValue;
+        if (currentTopValue.length < 3) {
+            console.log('insufficient formula')
+        } else {
+        let result = operate(currentTopValue);
+        screenMain.textContent = result;
+        }
     } else {
-        console.log('button was not a number')
+        console.log('no handler for this button yet')
+    }
+}
+
+function decimalPoint(number) {
+    let decimalAdded = number += ".";
+    currentMainValue = decimalAdded;
+    return screenMain.textContent = currentMainValue;
+}
+
+// https://dev.to/uzairsamad/how-to-check-if-number-is-float-in-js-h98
+function isFloat(n){
+    return Number(n) === n && n % 1 !== 0;
+  }
+
+function hasPeriod(str) {
+    if (str.includes('.')) {
+        console.log('float presented as string')
+    } else {
+        console.log('not a float')
     }
 }
 
@@ -94,8 +170,10 @@ function clear(number) {
         let newNumber = parseInt(newString);
         currentMainValue = newNumber;
         return screenMain.textContent = currentMainValue;
+    } else if (isFloat(number)) {
+        console.log("number is a floating point integer");
     } else {
-        return console.error("ERROR");
+        return console.error("no clear condition to handle this value");
     }
 };
 
